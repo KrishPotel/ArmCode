e74edc229a56a6aa627bf3459ed9fec0c66cb354
KrishPatel <krishpatelsuper@gmail.com>
2025-07-14 14:13:05 -0400

    Test Arm Code move servo to 90

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 import RPi.GPIO as GIPO
 import time
+
+GIPO.setmode(GIPO.BCM)
+GIPO.setup(14, GIPO.OUT)
+
+pwm = GIPO.PWN(17,50)
+pwm.start()
+
+def set_angle(angle):
+    duty = (0.05 * angle) + 2.5
+    pwm.ChangeDutyCycle(duty)
+    time.sleep(0.5);
+    pwm.ChangeDutyCycle(0);
+
+try:
+    while True:
+        set_angle(90)
+
+except KeyboardInterrupt:
+    pass
+finally:
+    pwm.stop()
+    GIPO.cleanup()
