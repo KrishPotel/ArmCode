a585cc2b0bc6b08c07fcbd671bd703702433c84c
KrishPatel <krishpatelsuper@gmail.com>
2026-04-09 15:37:46 -0400

    Fixes

--- main.py
+++ main.py
@@ -3,18 +3,25 @@ import time
 
 pi = pigpio.pi()
 
-def setAngle(angle,SERVO_PIN):
-    plusewidth = 500 + (angle / 180.0) * 2000
-    pi.set_servo_pulsewidth(SERVO_PIN,plusewidth)
+if not pi.connected:
+    print("pigpio daemon not running!")
+    exit()
+
+SERVO_PIN = 14
+
+def setAngle(angle):
+    angle = max(0, min(180, angle))  # clamp
+    pulsewidth = 500 + (angle / 180.0) * 2000
+    pi.set_servo_pulsewidth(SERVO_PIN, pulsewidth)
 
 try:
     while True:
-        angle = int(input("Enter Servo 1 angle"))
-        setAngle(angle,14)
+        angle = int(input("Enter servo angle (0–180): "))
+        setAngle(angle)
 
 except KeyboardInterrupt:
     pass
 
 finally:
-    pi.set_servo_pulsewidth(15,0)
+    pi.set_servo_pulsewidth(SERVO_PIN, 0)  # stop signal
     pi.stop()
