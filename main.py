a8aa8664915b7e8c88dca549a40d3fa24802a982
KrishPatel <krishpatelsuper@gmail.com>
2026-04-22 14:01:42 -0400

    Updated controls

--- main.py
+++ main.py
@@ -8,16 +8,41 @@ if not pi.connected:
     exit()
 
 SERVO_PIN = 14
+SERVO_PIN_SMALL = 15
+SERVO_PIN_GRIPPER = 18
 
-def setAngle(angle):
-    angle = max(0, min(180, angle))  # clamp
+def setAngleBIG(angle):
+    angle = max(30, min(150, angle))  # clamp
     pulsewidth = 500 + (angle / 180.0) * 2000
     pi.set_servo_pulsewidth(SERVO_PIN, pulsewidth)
 
+def setAngleSmall(angle):
+    angle = max(0, min(180, angle))  # clamp
+    pulsewidth = 500 + (angle / 180.0) * 2000
+    pi.set_servo_pulsewidth(SERVO_PIN_SMALL, pulsewidth)
+
+def setGripperOpenOrClose(open: bool):
+    if(open):
+        angle = 0
+        pulsewidth = 500 + (angle / 180.0) * 2000
+        pi.set_servo_pulsewidth(SERVO_PIN_SMALL, pulsewidth)
+    else:
+        angle = 90
+        pulsewidth = 500 + (angle / 180.0) * 2000
+        pi.set_servo_pulsewidth(SERVO_PIN_GRIPPER, pulsewidth)
+
 try:
     while True:
+
+        angle2 = int(input("Enter servo angle (0-180): "))
+        setAngleSmall(angle2)
+
         angle = int(input("Enter servo angle (0-180): "))
-        setAngle(angle)
+        setAngleBIG(angle)
+
+        openOrClose = bool(input("Enter servo angle (0-180): "))
+        setGripperOpenOrClose(openOrClose)
+
 
 except KeyboardInterrupt:
     pass
